76d76112bf2186e137eea134f316966468cb4010
Lukas Lieb <lukas.lieb@axonivy.com>
2026-04-13 13:43:09 +0200

    Eslint 10

--- packages/editor/src/context/ClientContext.tsx
+++ packages/editor/src/context/ClientContext.tsx
@@ -1,14 +1,14 @@
 import type { RoleClient } from '@axonivy/role-editor-protocol';
 import type { ReactNode } from 'react';
-import { createContext, useContext } from 'react';
+import { createContext, use } from 'react';
 
 export interface ClientContext {
   client: RoleClient;
 }
 
-const ClientContextInstance = createContext<ClientContext | undefined>(undefined);
+const ClientContext = createContext<ClientContext | undefined>(undefined);
 export const useClient = (): RoleClient => {
-  const context = useContext(ClientContextInstance);
+  const context = use(ClientContext);
   if (context === undefined) {
     throw new Error('useClient must be used within a ClientContext');
   }
@@ -16,5 +16,5 @@ export const useClient = (): RoleClient => {
 };
 
 export const ClientContextProvider = ({ client, children }: { client: RoleClient; children: ReactNode }) => {
-  return <ClientContextInstance.Provider value={{ client }}>{children}</ClientContextInstance.Provider>;
+  return <ClientContext value={{ client }}>{children}</ClientContext>;
 };
